9782c10d46d6ef365baef266f2e32eac8dbd8a0c
Daniel A <marckus22809@gmail.com>
2026-09-16 18:09:06 +0600

    Init project add music controller

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { createObserveModule } from '@nestjs/observe';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { MusicsController } from './musics/musics.controller.js';
+
+export const { ObserveModule, ObserveInstrument } = createObserveModule();
+
+@Module({
+  imports: [
+    ObserveModule.forRoot({
+      appKey: 'YOUR_APP_KEY',
+      appSecret: 'YOUR_APP_SECRET',
+      serviceId: 'music-api-nest',
+    }),
+  ],
+  controllers: [AppController, MusicsController],
+  providers: [AppService],
+})
+export class AppModule {}
